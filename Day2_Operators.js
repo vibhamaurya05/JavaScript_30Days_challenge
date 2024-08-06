cb1d116d03f928bae0684fe6ea028cdbdeeb8cf0
vibhamaurya05 <vibhamaurya5sep@gmail.com>
2024-08-06 17:54:17 +0530

    fix: Loops solutions

--- Day2_Operators.js
+++ Day2_Operators.js
@@ -1,26 +1,72 @@
 // ****************************************** Task 1-4 *************************************************
 // WAP to find sub,diifrence,product and division of two numbers and  log the output in console.
+let a=10;
+let b=30;
+let add= a+b;
+console.log("add:", add);
+console.log(`difference: ${b-a}`);
+console.log(`product: ${a*b}`);
+console.log(`division: ${b/a}`);
 
 
 // ****************************************** Task 5 *************************************************
 // WAP to find the reminder when a num is divided by other num.
+let c=25
+let d=6
+console.log(`remainder: ${c%d}`)
 
 //  +=, -= operator to add and substract a num from other num.
 // ****************************************** Task 6-7 *************************************************
+let g=25
+let h=6
+g+=h;
+console.log("add:",g);
+
+let e=25
+let f=5
+e-=f
+console.log("subtract:",e);
+
 
 //  use '<', '>', '<=' ,'=>', '==', '===, 
 // Coercion(implicit conversion of one type of data type to another) as 9 is implicitly converted string to num.
 
 // ****************************************** Task 8 *************************************************
-
+if(2<3)
+    console.log("2 is smaller than 3");
+else
+    console.log("2 is greater than 3")
 // ****************************************** Task 9 *************************************************
-
+if(4>3)
+    console.log("4 is greater than 3");
+else
+    console.log("4 is smaller than 3");
 // ****************************************** Task 10 *************************************************
+if(2<=3)
+    console.log("2 is greater than equals to 3");
+else
+    console.log("2 is smaller than equals to 3");
 
 // ****************************************** Task 11 *************************************************
-
+if(4>=3)
+    console.log("4 is smaller than equals to 3");
+else
+    console.log("4 is greater than equals to 3");
 // ****************************************** Task 12 *************************************************
+let x=10
+let y=10
+if(x==y)
+    console.log("x is equals to y.")
+else
+    console.log("x and y are not equal.")
 
 // ****************************************** Task 13 *************************************************
+let p=10
+let q="vibha"
+if(p===q)
+    console.log("p is strictly equals to q.")
+else
+    console.log("p and q are different or not strictly equal.")
+
 
 // ****************************************** Task 14 *************************************************
